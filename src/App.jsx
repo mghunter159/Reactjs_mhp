c223adbb1baefa7a403088811d0cd943c29112a8
mghunter159 <ya@mghunter.ru>
2023-04-23 12:40:01 +0500

    Urok3

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,7 @@ import { useState } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const handleInc = () => {
-    setCounter(counter+1);
-  }
-  const handleDec = () => {
-    //setCounter(counter-1);
-    if (counter <= 0) {
-      setCounter(0);
-    }
-    else {
-      setCounter(counter -1);
-    }
-  }
-  const handleRes = () => {
-    setCounter(0);
-  }
+
   return (
     <div className="App">
       <div className="container">
@@ -31,13 +17,13 @@ function App() {
             <div className="display-1 text-centr my-5 py-5">{counter}</div>
             <div className="row">
               <div className="col">
-                 <button className="btn btn-primaty w-100" onClick={handleInc}>Увеличить</button>
+                 <button className="btn btn-primaty w-100" onClick={() => setCounter(counter + 1)}>Увеличить</button>
               </div>
               <div className="col">
-                 <button className="btn btn-succes w-100" onClick={handleDec}>Уменьшить</button>
+                 <button className="btn btn-succes w-100" onClick={() => setCounter(counter - 1)}>Уменьшить</button>
               </div>
               <div className="col">
-                 <button className="btn btn-outline-danger w-100" onClick={handleRes}>Сбросить</button>
+                 <button className="btn btn-outline-danger w-100" onClick={() => setCounter(0)}>Сбросить</button>
               </div>
             </div>
           </div>
