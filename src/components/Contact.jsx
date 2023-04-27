320eb93930cd74748338c65cdd9919160465249c
mghunter159 <ya@mghunter.ru>
2023-04-27 17:17:26 +0500

    Urok 5

---
+++ src/components/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <>
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+                    <h1>Contact Page</h1>
+                </div>
+            </div>
+            
+            <div className='container'>
+                <pre> 
+                    Кошачья система, галактический рукав мисок, галактика еды, Местная группа котов, Дом милостей, 2 этаж, 1 маленькая дверь, диван.
+                </pre>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjdJIEf97fTYvxgVQe04m4EKP5pc0C-XA5Xw&usqp=CAU' className='center'></img>
+            </div>
+        </>
+    )
+}
